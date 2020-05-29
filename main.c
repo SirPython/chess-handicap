ef5b936a61f0d4793eeddd0bdc762daa2c6fe773
SirPython <SirPython@github.noreply.com>
2020-05-29 16:09:21 -0400

    it looks like info is getting assigned the same section in the stack each call so it retains all the info from last call... that is fucking WILD bro

--- main.c
+++ main.c
@@ -1,12 +1,6 @@
 #include "chess-handicap.h"
 
-/**
- * 1. Take a start position
- * 2. Have UCI engines play this position: one starts with modified pos, other
- *   start normal.
- * 3. Run ~100 games or something.
- * 4. Calculate odds of winning
- */
+void run_game(subproc uci, char *fen);
 
 int main(int argc, char **argv) {
     if(argc < 3) {
@@ -14,35 +8,43 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-
+    subproc uci;
+    load_ipc(argv[1], &uci);
 
     for(int i = 0; i < 10; i++) {
-        subproc uci;
-        load_ipc(argv[1], &uci);
-        uci_init(uci);
-        game g;
-        game_init(&g, argv[2]);
-
-        info_block info;
-        while(true) {
-            uci_calc(uci, g);
-            uci_read_info(uci, &info);
-            if(info.mate) {
-                printf("***** %s won.\n", g.n_moves % 2 == 0 ? "Black" : "White");
-                break;
-            } else
-            if(info.cp == 0 && info.move[0] == '(') { /* A draw was reached. */
-                puts("***** It's a draw.");
-                break;
-            }
-
-            printf("%s is playing %s\n", g.n_moves % 2 == 0 ? "Black" : "White", info.move);
-            game_play(&g, info.move);
-        }
-
-        kill_ipc(uci);
+        run_game(uci, argv[2]);
     }
+
+    kill_ipc(uci);
 
 
     return 0;
+}
+
+void run_game(subproc uci, char *fen) {
+    puts("how?");
+
+    uci_init(uci);
+    game g;
+    game_init(&g, fen);
+
+    info_block info;
+
+    printf("%d\n", info.mate);
+    while(true) {
+        puts("super brh");
+        uci_calc(uci, g);
+        uci_read_info(uci, &info);
+        if(info.mate) {
+            printf("***** %s won.\n", g.n_moves % 2 == 0 ? "Black" : "White");
+            break;
+        } else
+        if(info.cp == 0 && info.move[0] == '(') { /* A draw was reached. */
+            puts("***** It's a draw.");
+            break;
+        }
+
+        printf("%s is playing %s\n", g.n_moves % 2 == 0 ? "Black" : "White", info.move);
+        game_play(&g, info.move);
+    }
 }
