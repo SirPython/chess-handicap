b1521087d62a329882a54db83d84c82d92a89815
SirPython <SirPython@github.noreply.com>
2020-05-29 16:18:37 -0400

    calculates % odds... but it's still strange AF

--- main.c
+++ main.c
@@ -1,50 +1,57 @@
 #include "chess-handicap.h"
 
-void run_game(subproc uci, char *fen);
+double run_game(subproc uci, char *fen);
 
 int main(int argc, char **argv) {
-    if(argc < 3) {
-        printf("Use: %s [path to UCI engine] [starting position in FEN string]\n", argv[0]);
+    if(argc < 4) {
+        printf("Use: %s [path to UCI engine] [starting position in FEN string] [number of games]\n", argv[0]);
         return 1;
     }
 
     subproc uci;
     load_ipc(argv[1], &uci);
 
-    for(int i = 0; i < 10; i++) {
-        run_game(uci, argv[2]);
+    long n_games = strtol(argv[3], NULL, 10);
+    double white_score = 0.0;
+    for(int i = 0; i < n_games; i++) {
+        white_score += run_game(uci, argv[2]);
     }
 
-    kill_ipc(uci);
+    printf("White's score: %.2f/%ld\n", white_score, n_games);
 
+    kill_ipc(uci);
 
     return 0;
 }
 
-void run_game(subproc uci, char *fen) {
-    puts("how?");
+double run_game(subproc uci, char *fen) {
+    double ret;
 
     uci_init(uci);
     game g;
     game_init(&g, fen);
 
     info_block info;
-
-    printf("%d\n", info.mate);
     while(true) {
-        puts("super brh");
         uci_calc(uci, g);
         uci_read_info(uci, &info);
         if(info.mate) {
-            printf("***** %s won.\n", g.n_moves % 2 == 0 ? "Black" : "White");
+            ret = g.n_moves % 2 == 0 ? 0.0 : 1.0;
             break;
         } else
         if(info.cp == 0 && info.move[0] == '(') { /* A draw was reached. */
-            puts("***** It's a draw.");
+            ret = 0.5;
             break;
         }
 
-        printf("%s is playing %s\n", g.n_moves % 2 == 0 ? "Black" : "White", info.move);
         game_play(&g, info.move);
     }
+
+    /* It's wild. If I don't do this, info will represent the same block of
+     * memory next call. */
+    info.cp = 0;
+    info.mate = false;
+    free(info.move);
+
+    return ret;
 }
